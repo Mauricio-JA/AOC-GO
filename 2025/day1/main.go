f62235e79feb215ad773d86076c0ffafcee479af
Mauricio Justiniano Añez <mauricin.317@gmail.com>
2025-12-02 15:09:00 -0400

    (2025) day1 - part2

--- 2025/day1/main.go
+++ 2025/day1/main.go
@@ -13,15 +13,27 @@ func main() {
 	input, _ := os.ReadFile(inputPath)
 
 	var rotations = strings.Split(string(input), "\r\n")
-	dial, count := 50, 0
+	dial, count1, count2 := 50, 0, 0
 
 	for _, rotation := range rotations {
 		var pair = strings.SplitN(rotation, "", 2)
+		fmt.Println(pair, dial, count1, count2)
+
 		direction := pair[0]
 		value, _ := strconv.Atoi(pair[1])
 		if direction == "R" {
+
+			count2 += (dial + value) / 100
 			dial = (dial + value) % 100
+
 		} else {
+
+			if dial != 0 {
+				count2 += (100 - dial + value) / 100
+			} else {
+				count2 += (dial + value) / 100
+			}
+
 			if value%100 > dial {
 				dial = 100 + dial - (value % 100)
 			} else {
@@ -29,9 +41,10 @@ func main() {
 			}
 		}
 		if dial == 0 {
-			count++
+			count1++
 		}
 	}
 
-	fmt.Println("Part 1", count)
+	fmt.Println("Part 1", count1)
+	fmt.Println("Part 2", count2)
 }
